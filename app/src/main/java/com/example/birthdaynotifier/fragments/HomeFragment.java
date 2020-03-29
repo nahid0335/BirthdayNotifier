2d344a1b130e3c3ec99cd8dd3b0343ab792bbba9
Tamzid Hasan <nahid0335@gmail.com>
2020-03-29 13:12:21 +0600

    spinner complete

--- app/src/main/java/com/example/birthdaynotifier/fragments/HomeFragment.java
+++ app/src/main/java/com/example/birthdaynotifier/fragments/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.birthdaynotifier.fragments;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.birthdaynotifier.Adapter.BirthDateAdapter;
@@ -15,6 +20,9 @@ import com.example.birthdaynotifier.BirthDate;
 import com.example.birthdaynotifier.R;
 import com.example.birthdaynotifier.ViewModel.BirthDateViewModel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -34,12 +42,28 @@ public class HomeFragment extends Fragment {
     public static final int ADD_NEW_REQUEST = 1;
     public static final int EDIT_NEW_REQUEST = 2;
 
+
+    SharedPreferences sharedpreferences;
+    public static final String sharedPreferenceKey = "BirthdatePrivateKey";
+    public static final String sortTitle = "sortTitleKey";
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
 
         ImageView addNewItem = rootview.findViewById(R.id.imageView_mainHome_add);
+        final Spinner sortList = rootview.findViewById(R.id.spinner_mainHome_sortList);
+
+        sharedpreferences = getContext().getSharedPreferences(sharedPreferenceKey, Context.MODE_PRIVATE);
+        if(!sharedpreferences.contains(sortTitle)){
+            //Toast.makeText(getContext(),"sharedpreference",Toast.LENGTH_LONG).show();
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString(sortTitle, "Alphabetic");
+            editor.apply();
+        }
+
         addNewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +71,12 @@ public class HomeFragment extends Fragment {
                 startActivityForResult(intent, ADD_NEW_REQUEST);
             }
         });
+
+        final ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getContext(),R.array.mainHome_sortList,android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sortList.setAdapter(spinnerAdapter);
+
+
 
         RecyclerView recyclerView = rootview.findViewById(R.id.recyclerView_home_container);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
@@ -60,13 +90,88 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable List<BirthDate> birthDates) {
                 //update RecyclerView
-                //Toast.makeText(getContext(),"update",Toast.LENGTH_LONG).show();
+                String sortListName = sharedpreferences.getString(sortTitle,null);
+                sortList.setSelection(spinnerAdapter.getPosition(sortListName));
+                if(sortListName.equals("Date")){
+                    Collections.sort(birthDates, new Comparator<BirthDate>() {
+                        @Override
+                        public int compare(BirthDate t1, BirthDate t2) {
+                            if(t1.getMonth()<t2.getMonth()){
+                                return -1;
+                            }else if(t1.getMonth()>t2.getMonth()){
+                                return 1;
+                            }else{
+                                if(t1.getDay()<t2.getDay()){
+                                    return -1;
+                                }else{
+                                    return 1;
+                                }
+                            }
+                        }
+                    });
+                }else{
+                    Collections.sort(birthDates, new Comparator<BirthDate>() {
+                        @Override
+                        public int compare(BirthDate birthDate, BirthDate t1) {
+                            return birthDate.getName().compareToIgnoreCase(t1.getName());
+                        }
+                    });
+                }
                 adapter.submitList(birthDates);
-                //Toast.makeText(getContext(),"updatelater",Toast.LENGTH_LONG).show();
 
             }
         });
         //Toast.makeText(getContext(),"home",Toast.LENGTH_LONG).show();
+
+
+        sortList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                List<BirthDate>birthDates = birthDateViewModel.getAllBirthDate().getValue();
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                if(selectedItem.equals("Date")){
+                    editor.putString(sortTitle, "Date");
+                    editor.apply();
+
+                    Collections.sort(birthDates, new Comparator<BirthDate>() {
+                        @Override
+                        public int compare(BirthDate t1, BirthDate t2) {
+                            if(t1.getMonth()<t2.getMonth()){
+                                return -1;
+                            }else if(t1.getMonth()>t2.getMonth()){
+                                return 1;
+                            }else{
+                                if(t1.getDay()<t2.getDay()){
+                                    return -1;
+                                }else{
+                                    return 1;
+                                }
+                            }
+                        }
+                    });
+                }else{
+                    editor.putString(sortTitle, "Alphabetic");
+                    editor.apply();
+
+                    Collections.sort(birthDates, new Comparator<BirthDate>() {
+                        @Override
+                        public int compare(BirthDate birthDate, BirthDate t1) {
+                            return birthDate.getName().compareToIgnoreCase(t1.getName());
+                        }
+                    });
+                }
+                adapter.submitList(birthDates);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
 
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
@@ -154,4 +259,5 @@ public class HomeFragment extends Fragment {
             Toast.makeText(getContext(), "Birthday not saved", Toast.LENGTH_SHORT).show();
         }
     }
+
 }

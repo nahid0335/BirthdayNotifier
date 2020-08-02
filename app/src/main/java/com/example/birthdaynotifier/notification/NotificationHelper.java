6e1ff7c88cd5ab5763a7444eb8b880e7ce2a93de
Tamzid Hasan <nahid0335@gmail.com>
2020-08-02 23:57:49 +0600

    notification vibration

--- app/src/main/java/com/example/birthdaynotifier/notification/NotificationHelper.java
+++ app/src/main/java/com/example/birthdaynotifier/notification/NotificationHelper.java
@@ -1,13 +1,18 @@
 package com.example.birthdaynotifier.notification;
 
 import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
 import android.os.Build;
 
+import com.example.birthdaynotifier.MainActivity;
 import com.example.birthdaynotifier.R;
 
 import androidx.core.app.NotificationCompat;
@@ -33,6 +38,11 @@ public class NotificationHelper extends ContextWrapper {
     @TargetApi(Build.VERSION_CODES.O)
     private void createChannel() {
         NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        channel.enableVibration(true);
+        channel.enableLights(true);
+        channel.setVibrationPattern(new long[] { 100, 1000, 100, 1000, 100 });
+        channel.setLightColor(R.color.colorPrimary);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
 
         getManager().createNotificationChannel(channel);
     }
@@ -47,12 +57,18 @@ public class NotificationHelper extends ContextWrapper {
     }
 
     public NotificationCompat.Builder getChannelNotification(String text) {
+        Intent resultintent = new Intent(this, MainActivity.class);
+        PendingIntent resultpendingIntent = PendingIntent.getActivity(this,11604013,resultintent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         return new NotificationCompat.Builder(getApplicationContext(), channelID)
                 .setContentTitle("Happy Birthday To ")
                 .setContentText(text)
                 .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setAutoCancel(true)
+                .setContentIntent(resultpendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] { 100, 1000, 100, 1000, 100 })
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE);
     }
 }
